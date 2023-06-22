00079e4d988e393e2547c34df795e78d28c1b5f1
tandsilva <tamdsilva@logatti.edu.br>
2023-06-21 21:34:00 -0300

    first commit

---
+++ grpc-spring-course/grpc-spring-course/src/main/java/br/com/content4devs/resourses/HelloResourse.java
@@ -0,0 +1,25 @@
+package br.com.content4devs.resourses;
+
+import br.com.content4devs.HelloReq;
+import br.com.content4devs.HelloRes;
+import br.com.content4devs.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloResourse extends HelloServiceGrpc.HelloServiceImplBase {
+
+
+
+
+
+    @Override
+    public void hello(HelloReq request, StreamObserver<HelloRes> responseObserver) {
+        //padrao builder vai constrir os objetos de forma padrao internacional de projeto
+       var response = HelloRes.newBuilder()
+               .setMessage(request.getMessage())
+               .build();
+       responseObserver.onNext(response);
+       responseObserver.onCompleted();
+    }
+}
