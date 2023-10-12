88f57f28922475481dac0b170c444435b40a1922
Thiago Aldrin Marques da Silva <tamdsilva@logatti.edu.br>
2023-10-11 21:42:57 -0300

    Resources

---
+++ grpc-spring-course/grpc-spring-course/src/main/java/br/com/content4devs/Resources/ProductResource.java
@@ -0,0 +1,11 @@
+package br.com.content4devs.Resources;
+
+import br.com.content4devs.ProductServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class ProductResource extends ProductServiceGrpc.ProductServiceImplBase {
+
+
+
+}
