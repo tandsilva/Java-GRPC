58168f013e314256826697c5a57437db75fdf753
Thiago Aldrin Marques da Silva <tamdsilva@logatti.edu.br>
2023-10-26 20:14:25 -0300

    Oh go home

---
+++ grpc-spring-course/grpc-spring-course/src/test/java/br/com/content4devs/resources/ProductResourceIntregationTest.java
@@ -0,0 +1,12 @@
+package br.com.content4devs.resources;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductResourceIntregationTest {
+
+
+
+
+
+}
