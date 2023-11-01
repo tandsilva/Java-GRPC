1ef4ad74fcb92e1a25e4a10026cfcf52e3a9c69d
Thiago Aldrin Marques da Silva <tamdsilva@logatti.edu.br>
2023-11-01 18:33:34 -0300

    Configurando testes

--- grpc-spring-course/grpc-spring-course/src/test/java/br/com/content4devs/resources/ProductResourceIntregationTest.java
+++ grpc-spring-course/grpc-spring-course/src/test/java/br/com/content4devs/resources/ProductResourceIntregationTest.java
@@ -1,12 +1,46 @@
 package br.com.content4devs.resources;
 
+import br.com.content4devs.ProductRequest;
+import br.com.content4devs.ProductResponse;
+import br.com.content4devs.ProductServiceGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:applicationTest.properties")//esta chamando a configuracao do bnco aqui h2
+@DirtiesContext//para evitar problemas de portas locadas no teste
 public class ProductResourceIntregationTest {
 
+// soh eh possivel sdevido ao importe no pom Din jarin  (lembra)
+@GrpcClient("inProcess")
+//vai realizar chamadas blocantes
+    private ProductServiceGrpc.ProductServiceBlockingStub serviceBlockingStub;
+
+@Test
+@DisplayName("Se a data for valida um produto eh criado")
+    public  void  createProductSucessTest(){
+    //o metodo create precisa receber um product request
+
+
+   ProductRequest productRequest = ProductRequest.newBuilder()
+            .setName("product name")
+                    .setPrice(10.00)
+                    .setQuantityInStock(100)
+                    .build();
 
 
 
+         ProductResponse productResponse = serviceBlockingStub.create(productRequest);
+         //os nomes que estao sendo chamados aqui são os nomes do  arquivo protp
+        Assertions.assertThat(productRequest).usingRecursiveComparison().comparingOnlyFields("name","price","quantity_in_stock")
+            .isEqualTo(productResponse);
+
+
+}
 
 }
